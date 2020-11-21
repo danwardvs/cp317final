39507cbd0c639d048e6f93b4324d8bc05d45fef6
Danny Van Stemp <danwardvs@adsgames.net>
2020-11-21 14:15:07 -0500

    Showing up markers based on .json file

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,21 +1,34 @@
-import * as React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import * as React from "react";
+import { Button, Dimensions, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import data from "../data/stops.json";
+import { Stop } from "../types/Stop";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapStyle}
-      initialRegion={{
-        latitude:43.4643,
-        longitude:-80.5204,      
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }} 
-  />
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: 43.4643,
+          longitude: -80.5204,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
+        {data.allStops.map((stop: Stop, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: stop.lat, longitude: stop.long }}
+            title={stop.title}
+            description={stop.description}
+          />
+        ))}
+      </MapView>
+      <Button onPress={() => console.log("bum")} title={"Find Location"} />
     </View>
   );
 }
@@ -23,20 +36,18 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2,
   },
 });
