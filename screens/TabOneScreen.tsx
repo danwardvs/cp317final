5be5423d85a0602fff79316889c4f8bdfff720ec
Danny Van Stemp <danwardvs@adsgames.net>
2020-11-22 19:49:45 -0500

    Animate to location hitting "Find Location"

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -12,14 +12,26 @@ import * as Permissions from "expo-permissions";
 import { Coordinate } from "../types/Coordinate";
 
 export default function TabOneScreen() {
+  let mapRef = React.useRef<MapView>(null);
   const [userLocation, setUserLocation] = React.useState<Coordinate>();
   const [userGeocode, setUserGeocode] = React.useState<any>();
   const [details, setDetails] = React.useState<string>();
 
+  const changeRegion = (latitude: number, longitude: number) => {
+    mapRef?.current?.animateToRegion(
+      {
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      },
+      1000
+    );
+  };
+
   const getGeocodeAsync = async (location: any) => {
     let geocode = await Location.reverseGeocodeAsync(location);
     setUserGeocode(geocode);
-    console.log(geocode);
   };
   const getReadableAddress = (geocode: Location.LocationGeocodedAddress[]) => {
     let address = "";
@@ -47,14 +59,15 @@ export default function TabOneScreen() {
     });
     const { latitude, longitude } = location.coords;
     setUserLocation({ latitude, longitude });
-    getGeocodeAsync({ latitude, longitude });
+    changeRegion(latitude, longitude);
 
-    console.log(location);
+    getGeocodeAsync({ latitude, longitude });
   };
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.mapStyle}
         initialRegion={{
           latitude: 43.4643,
