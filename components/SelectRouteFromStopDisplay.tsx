e9b171d8526c8dc96d4e9f90d34fcc1d43b90426
Danny Van Stemp <danwardvs@adsgames.net>
2020-12-09 14:26:38 -0500

    Fixed schedule display

--- components/SelectRouteFromStopDisplay.tsx
+++ components/SelectRouteFromStopDisplay.tsx
@@ -11,47 +11,61 @@ import { getWeekday } from "../util/helpers";
 import { MonoText } from "./StyledText";
 import { Text, View } from "./Themed";
 import { lookupStopId, lookupRouteId } from "../util/helpers";
-import { Stop } from "../types/types"
+import { Stop } from "../types/types";
+import CustomButton from "./CustomButton";
 
 function TabBarIcon(props: { name: string; color: string }) {
-    return <AntDesign size={24} style={{ marginBottom: -3 }} {...props} />;
+  return <AntDesign size={24} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function ScheduleRouteFromStopDisplay({ stop }: { stop: Stop }) {
-    if (stop)
-        return (
-            <View style={styles.container}>
-            <FlatList
-                data={stop.routes}
-                renderItem={({ item }) => (
-                <TouchableOpacity
-                    onPress={() => {
-                        const route_to_display = lookupRouteId(stop.routes, item.stop_route_id)
-                        if (route_to_display)
-                            return (
-                                <View style={{ padding: 12 }}>
-                                    <ScheduleDisplay stop_route={route_to_display} />
-                                </View>
-                            )
-                    }}
-                >
-                    <View style={styles.tile} key={item.stop_route_id}>
-                    <Text style={styles.item}>{item.stop_route_id}</Text>
-                    <View style={{ paddingTop: 10 }}>
-                        <TabBarIcon name="right" color={"gray"} />
-                    </View>
-                    </View>
-                </TouchableOpacity>
-                )}
-            />
-            </View>
-        );
-    
+  const [selectedSchedule, setSelectedSchedule] = React.useState<
+    StopRoute | undefined
+  >();
+
+  if (selectedSchedule && typeof selectedSchedule !== "undefined") {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Error loading stop data: Invalid ID</Text>
-        </View>
+      <View style={{ padding: 12 }}>
+        <CustomButton
+          label={"Back"}
+          onPress={() => setSelectedSchedule(undefined)}
+        />
+        <ScheduleDisplay stop_route={selectedSchedule} />
+      </View>
     );
+  }
+  if (stop)
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={stop.routes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                const route_to_display = lookupRouteId(
+                  stop.routes,
+                  item.stop_route_id
+                );
+                if (route_to_display) setSelectedSchedule(route_to_display);
+              }}
+            >
+              <View style={styles.tile} key={item.stop_route_id}>
+                <Text style={styles.item}>{item.stop_route_id}</Text>
+                <View style={{ paddingTop: 10 }}>
+                  <TabBarIcon name="right" color={"gray"} />
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Error loading stop data: Invalid ID</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
